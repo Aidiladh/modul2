d683321fdd32a4c74bc52e3951e7d1b85be2d072
Aidiladh <aidila1684211@gmail.com>
2022-03-20 23:23:34 +0700

    percabangan switch

---
+++ src/PercabanganSwitch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PercabanganSwitch {
+    public static void main(String[] args) {
+
+        //membuat variabel dan Scanner
+        String lampu;
+        Scanner scan = new Scanner(System.in);
+
+        //mengambil input
+        System.out.print("lnputkan nama warna: ");
+        lampu = scan.nextLine();
+
+        switch(lampu) {
+            case "merah":
+                System.out.println("Lampu merah, berhenti!");
+                break;
+            case "kuning":
+                System.out.println("Lampu kuning, harap hati-hati!");
+                break;
+            case "hijau":
+                System.out.println("Lampu hijau, silahkan jalan!");
+                break;
+            default:
+                System.out.println("Warna lampu salah!");
+        }}}
