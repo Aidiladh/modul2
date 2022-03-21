17b4a2b180834a3c6d9041c98be6774f349b9c3a
Aidiladh <aidila1684211@gmail.com>
2022-03-21 19:42:40 +0700

    menggunakan perulangan for

---
+++ src/PerulanganFor.java
@@ -0,0 +1,11 @@
+import java.util .Scanner;
+public class PerulanganFor {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System .in);
+            System.out.print("Masukkan jum lah perulangan : ");
+            int jml = input.nextInt();
+            for(int i =0; i < jml ; i++)
+                System.out.println("Pemrograman Berorientasi Objek dengan Java!") ;
+
+        }}
