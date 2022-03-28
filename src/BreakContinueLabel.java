4792862f040c66d5e64f7ac7f6d3cce9a979254b
Aidiladh <aidila1684211@gmail.com>
2022-03-28 14:34:09 +0700

    penanganan eksepsi

---
+++ src/BreakContinueLabel.java
@@ -0,0 +1,24 @@
+public class BreakContinueLabel {
+
+    public static void main(String[] args) { int a, b;
+        System.out.println("Contoh break Label"); Mulai:
+        for (a = 0; a < 2; a++) { for (b = 0; b < 3; b++) {
+            if (b == 1) {
+                break Mulai;
+            }
+                System.out.println("a=" + a + " ; b=" + b);
+            }
+        }
+
+            System.out.println("\nContoh continue Label");
+            Lanjut:
+            for (a = 0; a < 2; a++) {
+                for (b = 0; b < 3; b++) {
+                    if (b == 1) {
+                        continue Lanjut;
+                    }
+                    System.out.println("a=" + a + " ; b=" + b);
+                }
+            }
+
+            }}
